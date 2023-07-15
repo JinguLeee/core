60a18f4559fa3fc2f59083b90136052e56ec0abf
JinguLeee <pingu514@naver.com>
2023-07-15 22:38:08 +0900

    새로운 할인 정책 개발 및 적용과 문제점

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +10,10 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); // DIP 위반
+    // DIP를 위반하지 않도록 인터페이스에만 의존하도록 의존관계를 변경
+    // private DiscountPolicy discountPolicy; => 구현체가 없어 null pointer exception 발생
+
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
