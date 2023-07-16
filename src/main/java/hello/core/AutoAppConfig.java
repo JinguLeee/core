24bc826211a3e089b820e0d380293df452e6a92a
JinguLeee <pingu514@naver.com>
2023-07-17 03:15:08 +0900

    중복 등록과 충돌

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -7,12 +10,17 @@ import org.springframework.context.annotation.FilterType;
 @Configuration
 @ComponentScan(
         // 탐색할 패키지의 시작 위치를 지정
-        basePackages = "hello.core.member",
+        // basePackages = "hello.core.member",
         // 지정한 클래스의 패키지를 탐색 시작 위치로 지정
-        basePackageClasses = AutoAppConfig.class,
+        // basePackageClasses = AutoAppConfig.class,
         // 기존 예제 코드 제외
         excludeFilters = @ComponentScan.Filter(type =  FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
 
+    // 수동 빈이 우선권을 가진다. (자동 빈을 오버라이딩 해버린다.) application.properties에서 오버라이딩을 막을 수 있다. 막혀 있는게 기본 값
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
