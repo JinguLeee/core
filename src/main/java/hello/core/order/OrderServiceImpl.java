8339fccc185212b3b9461dfb1388c739e078061e
JinguLeee <pingu514@naver.com>
2023-07-17 23:40:51 +0900

    애노테이션 직접 만들기

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,49 +1,24 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    // 조회 빈이 2개 이상 - 문제
-
-    /*
-     * 1. @Autowired 필드 명 매칭
-     * 타입 매칭 -> 결과가 2개 이상일 때, 필드 명, 파라미터 명으로 빈 이름 매칭
-     */
-    // @Autowired
-    // public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy rateDiscountPolicy) {
-    //     this.memberRepository = memberRepository;
-    //     this.discountPolicy = rateDiscountPolicy;
-    // }
-
-    /*
-     * 2. 추가 구분자 - @Qualifier 사용
-     * @Qualifier끼리 매칭 -> 없으면 빈 이름 매칭 -> 없으면 NoSuchBeanDefinitionException 예외 발생
-     */
-    // @Autowired
-    // public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) {
-    //     this.memberRepository = memberRepository;
-    //     this.discountPolicy = discountPolicy;
-    // }
-
-    /*
-     * 3. 우선순위를 - @Primary
-     * 우선순위를 가질 구현체에 @Primary를 붙임
-     */
-    // @Autowired
-    // public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-    //     this.memberRepository = memberRepository;
-    //     this.discountPolicy = discountPolicy;
-    // }
+     @Autowired
+     public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
+         this.memberRepository = memberRepository;
+         this.discountPolicy = discountPolicy;
+     }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
