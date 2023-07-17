31dced4627e80d7f931f195744b9b556992e46d9
JinguLeee <pingu514@naver.com>
2023-07-17 21:05:23 +0900

    옵션 처리

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,8 +1,5 @@
 package hello.core;
 
-import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -19,8 +16,8 @@ import org.springframework.context.annotation.FilterType;
 public class AutoAppConfig {
 
     // 수동 빈이 우선권을 가진다. (자동 빈을 오버라이딩 해버린다.) application.properties에서 오버라이딩을 막을 수 있다. 막혀 있는게 기본 값
-    @Bean(name = "memoryMemberRepository")
-    MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-    }
+    //    @Bean(name = "memoryMemberRepository")
+    //    MemberRepository memberRepository() {
+    //        return new MemoryMemberRepository();
+    //    }
 }
