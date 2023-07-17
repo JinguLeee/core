fb961d3e14132f86f181b316a5304ca280874f38
JinguLeee <pingu514@naver.com>
2023-07-17 22:44:28 +0900

    조회 빈이 2개 이상 - @Autowired 필드 명, @Qualifier, @Primary

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -13,11 +13,37 @@ public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-//    @RequiredArgsConstructor 가 생성자를 알아서 만들어준다.
-//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    }
+    // 조회 빈이 2개 이상 - 문제
+
+    /*
+     * 1. @Autowired 필드 명 매칭
+     * 타입 매칭 -> 결과가 2개 이상일 때, 필드 명, 파라미터 명으로 빈 이름 매칭
+     */
+    // @Autowired
+    // public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy rateDiscountPolicy) {
+    //     this.memberRepository = memberRepository;
+    //     this.discountPolicy = rateDiscountPolicy;
+    // }
+
+    /*
+     * 2. 추가 구분자 - @Qualifier 사용
+     * @Qualifier끼리 매칭 -> 없으면 빈 이름 매칭 -> 없으면 NoSuchBeanDefinitionException 예외 발생
+     */
+    // @Autowired
+    // public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) {
+    //     this.memberRepository = memberRepository;
+    //     this.discountPolicy = discountPolicy;
+    // }
+
+    /*
+     * 3. 우선순위를 - @Primary
+     * 우선순위를 가질 구현체에 @Primary를 붙임
+     */
+    // @Autowired
+    // public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    //     this.memberRepository = memberRepository;
+    //     this.discountPolicy = discountPolicy;
+    // }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
